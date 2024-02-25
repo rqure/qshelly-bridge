d888ccaab8815652d682da3ce9b11df89c993e1d
rqure <64617806+rqure@users.noreply.github.com>
2024-02-25 21:17:47 +0000

    some progress

--- go.mod
+++ go.mod
@@ -2,11 +2,17 @@ module github.com/rqure/qlogger
 
 go 1.21.6
 
-require github.com/rqure/qmq v1.0.18
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.3
+	github.com/rqure/qmq v1.0.20
+	google.golang.org/protobuf v1.32.0
+)
 
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/redis/go-redis/v9 v9.4.0 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/redis/go-redis/v9 v9.5.1 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,18 +6,59 @@ import (
 	"strconv"
 	"time"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	qmq "github.com/rqure/qmq/src"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
-	app := qmq.NewQMQApplication("qmq2mqtt")
-	app.Initialize()
-	defer app.Deinitialize()
+	mqttAddr := os.Getenv("MQTT_ADDR")
+	if mqttAddr == "" {
+		mqttAddr = "mosquitto"
+	}
+
+	defaultProducerLength, err := strconv.Atoi(os.Getenv("QMQ_DEFAULT_PRODUCER_LENGTH"))
+	if err != nil {
+		defaultProducerLength = 10
+	}
 
 	tickRateMs, err := strconv.Atoi(os.Getenv("TICK_RATE_MS"))
 	if err != nil {
 		tickRateMs = 100
 	}
+
+	app := qmq.NewQMQApplication("qmq2mqtt")
+	app.Initialize()
+	defer app.Deinitialize()
+	app.AddConsumer("qmq2mqtt:queue").Initialize()
+
+	opt := mqtt.NewClientOptions()
+	opt.AddBroker(mqttAddr)
+	client := mqtt.NewClient(opt)
+	client.Connect()
+	defer client.Disconnect(0)
+
+	client.Subscribe("#", 2, func(c mqtt.Client, m mqtt.Message) {
+		key := "qmq2mqtt:exchange:" + m.Topic()
+
+		p := app.Producer(key)
+		if p != nil {
+			p = app.AddProducer(key)
+			p.Initialize(int64(defaultProducerLength))
+		}
+
+		msg := &qmq.QMQMqttMessage{
+			Topic:     m.Topic(),
+			Payload:   m.Payload(),
+			Id:        uint32(m.MessageID()),
+			Qos:       int32(m.Qos()),
+			Retained:  m.Retained(),
+			Duplicate: m.Duplicate(),
+		}
+
+		app.Logger().Debug("Received MQTT message: " + protojson.Format(msg))
+		p.Push(msg)
+	})
 
 	sigint := make(chan os.Signal, 1)
 	signal.Notify(sigint, os.Interrupt)
@@ -29,7 +70,23 @@ func main() {
 			app.Logger().Advise("SIGINT received")
 			return
 		case <-ticker.C:
+			if !client.IsConnected() {
+				continue
+			}
 
+			for {
+				msg := &qmq.QMQMqttMessage{}
+
+				popped := app.Consumer("qmq2mqtt:queue").Pop(msg)
+				if popped == nil {
+					break
+				}
+
+				app.Logger().Debug("Sending MQTT message: " + protojson.Format(msg))
+				client.Publish(msg.Topic, byte(msg.Qos), msg.Retained, msg.Payload)
+
+				popped.Ack()
+			}
 		}
 	}
 }
