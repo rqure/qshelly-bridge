ded50220a1fd8ac0f95ee1facd5fbf68797a9e40
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-07-07 21:38:44 -0600

    some progress

--- devices/Aqara_LLKZMK12LM.go
+++ devices/Aqara_LLKZMK12LM.go
@@ -1,4 +1,18 @@
 package devices
 
+import (
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	qdb "github.com/rqure/qdb/src"
+)
+
 type Aqara_LLKZMK12LM struct {
+}
+
+func (d *Aqara_LLKZMK12LM) ProcessMessage(message mqtt.Message, db qdb.IDatabase) {
+}
+
+func (d *Aqara_LLKZMK12LM) ProcessNotification(notification *qdb.DatabaseNotification) {
+}
+
+func (d *Aqara_LLKZMK12LM) RegisterNotification(config *qdb.DatabaseNotificationConfig) {
 }
--- devices/Aqara_MCCGQ11LM.go
+++ devices/Aqara_MCCGQ11LM.go
@@ -1,4 +1,18 @@
 package devices
 
+import (
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	qdb "github.com/rqure/qdb/src"
+)
+
 type Aqara_MCCGQ11LM struct {
+}
+
+func (d *Aqara_MCCGQ11LM) ProcessMessage(message mqtt.Message, db qdb.IDatabase) {
+}
+
+func (d *Aqara_MCCGQ11LM) ProcessNotification(notification *qdb.DatabaseNotification) {
+}
+
+func (d *Aqara_MCCGQ11LM) RegisterNotification(config *qdb.DatabaseNotificationConfig) {
 }
