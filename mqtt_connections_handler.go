20817502aa97c01a2091160d7374c2893bfd23a9
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-07-07 21:15:16 -0600

    some progress

--- mqtt_connections_handler.go
+++ mqtt_connections_handler.go
@@ -1,4 +1,42 @@
 package main
 
+import qdb "github.com/rqure/qdb/src"
+
 type MqttConnectionsHandler struct {
+	db       qdb.IDatabase
+	isLeader bool
+}
+
+func NewMqttConnectionsHandler(db qdb.IDatabase) *MqttConnectionsHandler {
+	return &MqttConnectionsHandler{
+		db: db,
+	}
+}
+
+func (h *MqttConnectionsHandler) OnSchemaUpdated() {
+
+}
+
+func (h *MqttConnectionsHandler) OnBecameLeader() {
+	h.isLeader = true
+}
+
+func (h *MqttConnectionsHandler) OnLostLeadership() {
+	h.isLeader = false
+}
+
+func (h *MqttConnectionsHandler) Init() {
+
+}
+
+func (h *MqttConnectionsHandler) Deinit() {
+
+}
+
+func (h *MqttConnectionsHandler) DoWork() {
+
+}
+
+func (h *MqttConnectionsHandler) ProcessNotifications() {
+
 }
