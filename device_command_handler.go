3ad8652628406dd381407f9d8d4f2754f600df21
rqure <64617806+rqure@users.noreply.github.com>
2024-07-10 23:29:57 +0000

    some progress

---
+++ device_command_handler.go
@@ -0,0 +1,42 @@
+package main
+
+import qdb "github.com/rqure/qdb/src"
+
+type DeviceCommandHandler struct {
+	db       qdb.IDatabase
+	isLeader bool
+}
+
+func NewDeviceSynchronizer(db qdb.IDatabase) *DeviceCommandHandler {
+	return &DeviceCommandHandler{
+		db: db,
+	}
+}
+
+func (h *DeviceCommandHandler) OnSchemaUpdated() {
+
+}
+
+func (h *DeviceCommandHandler) OnBecameLeader() {
+	h.isLeader = true
+}
+
+func (h *DeviceCommandHandler) OnLostLeadership() {
+	h.isLeader = false
+}
+
+func (h *DeviceCommandHandler) Init() {
+
+}
+
+func (h *DeviceCommandHandler) Deinit() {
+
+}
+
+func (h *DeviceCommandHandler) DoWork() {
+
+}
+
+func (h *DeviceCommandHandler) ProcessNotification(notification *qdb.DatabaseNotification) {
+
+}
